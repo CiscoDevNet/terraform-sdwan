7fdef32dd9f64989f48deedf54b6e318a99d143d
Tim Thomas <tithomas@cisco.com>
2020-02-28 16:56:52 -0500

    Fill in type constraints

--- vmware/cedge_vmware/variables.tf
+++ vmware/cedge_vmware/variables.tf
@@ -1,14 +1,30 @@
-variable "datacenter" {}
-variable "cluster" {}
-variable "datastore" {}
-variable "resource_pool" {}
-variable "template" {}
-variable "vm_num_cpus" {}
-variable "vm_memory" {}
+variable "datacenter" {
+  type = string
+}
+variable "cluster" {
+  type = string
+}
+variable "datastore" {
+  type = string
+}
+variable "resource_pool" {
+  type = string
+}
+variable "template" {
+  type = string
+}
+variable "vm_num_cpus" {
+  type = number
+}
+variable "vm_memory" {
+  type = number
+}
 variable "vm_add_disks" {
   type = list(number)
 }
-variable "vm_thin_provisioned" {}
+variable "vm_thin_provisioned" {
+  type = bool
+}
 variable "device_list" {
-  type = any
+  type = map(object({}))
 }
