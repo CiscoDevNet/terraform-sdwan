6ccbde7accde9c970c40e26012eb9c2ae7a62d8f
Lori Jakab <lojakab@cisco.com>
2022-07-19 09:20:34 +0200

    Avoid AWS provider warning due to changed syntax

--- aws/Provision_Instances/aws.tf
+++ aws/Provision_Instances/aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "terraform"
-  region                  = "${data.terraform_remote_state.spam.outputs.region}"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "terraform"
+  region                   = "${data.terraform_remote_state.spam.outputs.region}"
 }
--- aws/Provision_VPC/aws.tf
+++ aws/Provision_VPC/aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "terraform"
-  region                  = "${var.region}"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "terraform"
+  region                   = "${var.region}"
 }
